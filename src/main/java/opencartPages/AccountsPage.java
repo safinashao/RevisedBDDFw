8cbd021b8dae3b86756005aa930ea7f4fbccfb5b
safinashao <talyshao@gmail.com>
2023-08-26 10:15:54 -0500

    added accounts page

---
+++ src/main/java/opencartPages/AccountsPage.java
@@ -0,0 +1,16 @@
+package opencartPages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AccountsPage {
+	
+	private WebDriver driver;
+	
+	public AccountsPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+	
+
+}
